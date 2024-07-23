02c2c6b85e26bfe98cdc9f7ba6fd5658b03c9ae3
r0vshan <rovshan.tamirov@gmail.com>
2024-07-23 12:46:01 +0400

    Minor changes to the code. Deleted useless functionality

--- src/main/java/com/example/todolist/ToDoListApplication.java
+++ src/main/java/com/example/todolist/ToDoListApplication.java
@@ -3,12 +3,22 @@ package com.example.todolist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Date;
+import java.util.Scanner;
+import java.sql.Connection;
+
 @SpringBootApplication
 public class ToDoListApplication {
 
+	static Scanner input = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoListApplication.class, args);
-		System.out.println("Hello World!");
+
+		ConnectionUtil db = new ConnectionUtil();
+
+		Connection conn = db.connect_to_db("postgres", "postgres", "mysecretpassword");
+
 	}
 
 }
