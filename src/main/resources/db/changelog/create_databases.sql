50721975afb5012826cd6bd130b378bcf79543ce
r0vshan <rovshan.tamirov@gmail.com>
2024-07-23 12:44:57 +0400

    Project Dependencies Added.

---
+++ src/main/resources/db/changelog/create_databases.sql
@@ -0,0 +1,39 @@
+CREATE TYPE task_status AS ENUM ('Not Done', 'In Progress', 'Done');
+
+create table if not exists public.users
+(
+    id       uuid default gen_random_uuid() not null
+        primary key,
+    username varchar(255)                   not null
+        unique,
+    password varchar(255)                   not null,
+    email    varchar(255)                   not null
+        unique
+);
+
+alter table public.users
+    owner to postgres;
+
+create table if not exists public.tasks
+(
+    id          serial
+    primary key,
+    title       varchar(255) not null,
+    description text,
+    status      varchar(50)
+    constraint tasks_status_check
+    check ((status)::text = ANY
+((ARRAY ['Not Done'::character varying, 'In Progress'::character varying, 'Done'::character varying])::text[])),
+    start_date  date         not null,
+    end_date    date,
+    user_id     uuid         not null
+    constraint fk_user
+    references public.users
+    );
+
+alter table public.tasks
+    owner to postgres;
+
+
+
+    
