3ec9d971f9c211f9bc03c57868fa8c9055b77cb6
r0vshan <rovshan.tamirov@gmail.com>
2024-07-25 11:33:21 +0400

    Added Getters and Setters.

---
+++ src/main/java/com/example/todolist/user/User.java
@@ -0,0 +1,49 @@
+package com.example.todolist.user;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String username;
+    private String password;
+    private String email;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
