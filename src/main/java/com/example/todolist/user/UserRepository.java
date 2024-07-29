436029182107fb928e9dd34d4cea89f8409cc6c2
r0vshan <rovshan.tamirov@gmail.com>
2024-07-29 10:15:50 +0400

    Final To-Do API implemented.

--- src/main/java/com/example/todolist/user/UserRepository.java
+++ src/main/java/com/example/todolist/user/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.todolist.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
 }
