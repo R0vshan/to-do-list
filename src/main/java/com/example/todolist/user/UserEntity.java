e641b62357110aedf16c683418665a56fc5de0b0
r0vshan <rovshan.tamirov@gmail.com>
2024-08-08 14:37:11 +0400

    Changed UserEntity.

---
+++ src/main/java/com/example/todolist/user/UserEntity.java
@@ -0,0 +1,29 @@
+package com.example.todolist.user;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+    private String username;
+    private String password;
+    private String email;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private List<Role> roles = new ArrayList<>();
+
+}
