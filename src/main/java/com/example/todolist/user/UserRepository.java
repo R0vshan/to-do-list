5bad8698951d7d526182787a47ee458d47dd2da5
r0vshan <rovshan.tamirov@gmail.com>
2024-08-08 14:38:33 +0400

    Added Roles To Code.

--- src/main/java/com/example/todolist/user/UserRepository.java
+++ src/main/java/com/example/todolist/user/UserRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, UUID> {
-    Optional<User> findByUsername(String username);
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+    Optional<UserEntity> findByUsername(String username);
 }
